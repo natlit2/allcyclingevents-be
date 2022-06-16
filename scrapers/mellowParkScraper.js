ef1ffc985bcb55b5ef34260dbb151f5e0ff2c11e
anton litvak <anton.k.litvak@gmail.com>
2022-06-16 20:13:07 +0200

    scrape  link mellowpark

--- scrapers/mellowParkScraper.js
+++ scrapers/mellowParkScraper.js
@@ -12,6 +12,11 @@ async function scrapeEvent(url) {
     '//*[@id="events"]/div/div/div[1]/div[3]/div/h3/a'
   );
 
+  //   const allLinks = await page.evaluate(() =>
+  //     document.querySelectorAll("h3 > a[href]")
+  //   );
+  //   console.log(allLinks);
+
   //get the title -> this works
   const txt = await titleElement.getProperty("textContent");
   const eventTitle = await txt.jsonValue();
@@ -19,10 +24,12 @@ async function scrapeEvent(url) {
   console.log(`the event title is: ${eventTitle}`);
   //////scrape the link to the event
 
-  ////to get 1 specific link -> this does not work need to fix the selector
-  const selector = "a.getAttribute('href')";
-  const eventLink = await page.$eval(selector, (el) => el.href);
+  ////to get 1 specific link
+  const linkSelector =
+    "#events > div > div > div.mod_eventlist.col-xs-12.block > div.event.layout_short.col-md-4.col-xs-12.col-sm-6.upcoming.even.first.last.cal_2 > div > h3 > a";
+  const eventLink = await page.$eval(linkSelector, (el) => el.href);
   console.log(`the event link is: ${eventLink}`);
+  //console.log(eventLink);
 
   //// to get all links on the page
 
