cf51e25d50a667ba9c4b9edec31bcd3305b0cb45
anton litvak <anton.k.litvak@gmail.com>
2022-07-04 01:58:18 +0200

    fix cm scraper

--- scrapers/CMscraper.js
+++ scrapers/CMscraper.js
@@ -1,6 +1,6 @@
 const puppeteer = require("puppeteer");
 const Event = require("../models/eventModel");
-//const connectDB = require("../dbinit");
+const connectDB = require("../dbinit");
 const moment = require("moment");
 
 async function scrapeEvent(url) {
@@ -8,66 +8,57 @@ async function scrapeEvent(url) {
   const page = await browser.newPage();
   try {
     await page.goto(url);
-  } catch (err) {
-    console.log(`Oooops...there was an Error on the main events page: ${err}`);
-  }
+    await page.waitForSelector(".container", { visible: true });
+    await page.screenshot({ path: "cm.png" });
+    console.log("the page is up");
 
-  //////scrape the link to the event
+    ////to get 1 specific link
+    const linkSelector = await page.waitForXPath(
+      "/html/body/div[3]/div[2]/div[2]/div[2]/div/div/div[2]/div[1]/div/div/a"
+    );
+    const eventLink = await linkSelector.getProperty("href");
+    const link = await eventLink.toString();
+    console.log(`the event link: ${link}`);
 
-  // // to get all links on the page
-  // const selector = "a.item-link";
-  // const eventLinks = await page.$$eval(selector, (am) =>
-  //   am.filter((e) => e.href).map((e) => e.href)
-  // );
-  //console.log(`ALL LINKS: ${eventLinks}`);
+    // const Urls = Array.from(eventLink).map((el) => el.href);
+    // console.log(`the event link is: ${Urls}`);
 
-  ////to get 1 specific link
-  const selector =
-    "body > div:nth-child(4) > div:nth-child(3) > div.col-md-3 > div:nth-child(3) > div > div > div.panel-body > div:nth-child(1) > div > div > a";
-  const eventLink = await page.$eval(selector, (el) => el.href);
-  console.log(`the event link is: ${eventLink}`);
+    //scrape the image link
+    const imglinkSelector =
+      "body > div:nth-child(4) > div:nth-child(3) > div.col-md-9 > div:nth-child(3) > div > div:nth-child(7) > div > a > img";
+    const imgLink = await page.$eval(imglinkSelector, (el) => el.src);
+    console.log(`IMAGE LINK IS: ${imgLink}`);
 
-  //scrape the image link
-  const imglinkSelector =
-    "body > div:nth-child(4) > div:nth-child(3) > div.col-md-9 > div:nth-child(3) > div > div:nth-child(3) > div > a > img";
-  const imgLink = await page.$eval(imglinkSelector, (el) => el.src);
-  console.log(`IMAGE LINK IS: ${imgLink}`);
+    //scrape the date element
+    const [dateElement] = await page.$x(
+      "/html/body/div[3]/div[2]/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/dl/dd"
+    );
+    const datetime = await dateElement.getProperty("textContent");
+    const eventDate = await datetime.jsonValue();
+    console.log(`THE EVENT DATE IS: ${eventDate}`);
 
-  //scrape the date element
-  const [dateElement] = await page.$x(
-    "/html/body/div[3]/div[2]/div[2]/div[3]/div/div/div[2]/div[2]/div[1]/dl/dd"
-  );
-  const datetime = await dateElement.getProperty("textContent");
-  const eventDate = await datetime.jsonValue();
-  console.log(`THE EVENT DATE IS: ${eventDate}`);
+    // //format the date
 
-  // //format the date
+    // const formatDate = (str) => {
+    //   const toArr = str.split(".");
+    //   return `${toArr[2]}-${toArr[1]}-${toArr[0].replace(".", "")}`;
+    // };
 
-  // const formatDate = (str) => {
-  //   const toArr = str.split(".");
-  //   return `${toArr[2]}-${toArr[1]}-${toArr[0].replace(".", "")}`;
-  // };
+    //format the evetn date! use moment?
+    const formatedDate = moment(eventDate, "DD.MM.YYYY").format();
+    console.log(`THE FORMATED DATE IS : ${formatedDate}`);
 
-  //format the evetn date! use moment?
-  const formatedDate = moment(eventDate, "DD.MM.YYYY").format();
-  console.log(`THE FORMATED DATE IS : ${formatedDate}`);
+    // // add 2 hours to the formated date to create the endDate
+    const endDate = moment(formatedDate).add(2, "h");
 
-  // add 2 hours to the formated date to create the endDate
-  const endDate = moment(formatedDate).add(2, "h");
+    console.log("end:", endDate);
 
-  console.log("end:", endDate);
-
-  //for await (const eventLink of eventLinks) {
-
-  const eventPage = await browser.newPage();
-  try {
-    await eventPage.goto(eventLink);
+    //for await (const eventLink of eventLinks) {
 
     //scrape the title
-    const [titleElement] = await eventPage.$x(
-      "/html/body/div[3]/div[2]/div/h1"
+    const [titleElement] = await page.$x(
+      "/html/body/div[3]/div[2]/div[2]/div[2]/div/div/div[2]/div[1]/div/div/a/h3/text()"
     );
-
     const txt = await titleElement.getProperty("textContent");
     const eventTitle = await txt.jsonValue();
 
@@ -84,7 +75,7 @@ async function scrapeEvent(url) {
       title: eventTitle,
       start: formatedDate,
       end: endDate,
-      link: eventLink,
+      link: link,
       imgLink: imgLink,
     });
     console.log(`New event created with id ${newEvent._id}`);
